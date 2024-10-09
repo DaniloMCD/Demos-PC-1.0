8059df8b274e55b6cc596265abdd49e258e1825c
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2024-10-08 23:36:50 -0600

    :boom: Boceto de Home y el carrusel de img

---
+++ Start.js
@@ -0,0 +1,46 @@
+const btnLeft = document.querySelector(".btn-left"),
+    btnRight = document.querySelector(".btn-right"),
+    slider = document.querySelector("#slider"),
+    sliderSection = document.querySelectorAll(".slider-section");
+
+    btnLeft.addEventListener("click", e => moveToLeft())
+    btnRight.addEventListener("click", e => moveToRight())
+
+    setInterval(() =>{
+        moveToRight()
+    },10000)
+
+    let operation = 0,
+        counter = 0,
+        widthImg = 100 / sliderSection.length;
+
+
+   function moveToRight(){
+    if(counter >=  sliderSection.length-1){
+        counter = 0;
+        operation = 0;
+        slider.style.transform = `translate(-${operation}%)`
+        slider.style.transition = "none"
+        return;
+    } 
+    counter++;
+    operation = operation + widthImg;
+    slider.style.transform = `translate(-${operation}%)`
+    slider.style.transition = "all ease .6s"
+    
+   }
+
+   function moveToLeft(){
+    counter--
+    if (counter < 0){
+        counter = sliderSection.length-1;
+        operation = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operation}%)`
+        slider.style.transition = "none"
+    } else {
+        operation = operation - widthImg;
+        slider.style.transform = `translate(-${operation}%)`
+        slider.style.transition = "all ease .6s"
+    }
+    
+   }
